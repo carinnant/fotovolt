e076a19ef0975dcd0ad82814e2f26048d96c911d
carinnant <83100284+carinnant@users.noreply.github.com>
2021-05-31 17:54:20 -0300

    Add files via upload

---
+++ Library/Collab/Download/Assets/Scripts/Interact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    private GameObject Mesa;
+
+    void Start()
+    {
+        Mesa = GetComponent<GameObject>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayPosition, Vector2.zero, 100);
+
+            if (hit && hit.transform.tag == "Interactable")
+            {
+                hit.transform.GetComponent<IInteractable>().Interact(Mesa);
+            }
+        }
+
+
+    }
+}
---
+++ Library/Collab/Download/Assets/Scripts/ZoomObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomObject : MonoBehaviour, IInteractable
+{
+    public float ZoomRatio = .5f;
+
+    public void Interact(GameObject Mesa)
+    {
+        Camera.main.orthographicSize *= ZoomRatio;
+        
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+}
