07c33192cfeccaefa1112c93bb079d59ca74ca6d
carinnant <83100284+carinnant@users.noreply.github.com>
2021-05-31 16:52:46 -0300

    Add files via upload

---
+++ Assets/moveMirror.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveMirror : MonoBehaviour {
+
+	//private Camera myCam;
+
+	public GameObject manivela;
+
+	void Start()
+	{
+		//myCam = Camera.main;
+	}
+
+	void Update()
+	{
+			this.transform.eulerAngles = new Vector3 (0, 0, manivela.transform.eulerAngles.z);
+	}
+}
