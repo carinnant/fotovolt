676076798f4111cf28162faf963bfb08489b3ef3
carinnant <83100284+carinnant@users.noreply.github.com>
2021-05-31 17:10:42 -0300

    Add files via upload

---
+++ Assets/Scripts/Experiment/Slider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class Slider : MonoBehaviour
+{
+    public Slider mainSlider;
+
+    private GameObject exp;
+
+    public float sValue;
+    public float newValue;
+
+
+    public UnityEngine.UI.Slider.SliderEvent onValueChanged;
+
+    public void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        mainSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        exp = GameObject.Find("Image");
+
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        sValue = newValue;
+        Debug.Log(mainSlider.newValue);
+    }
+}
---
+++ Assets/Scripts/Experiment/questexperimento.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class questexperimento : MonoBehaviour
+{
+	public float time = 30.0f;
+	public Text tempo;
+	public int ctime;
+
+	public static float[] value = new float[1];
+	public static bool[] button = new bool[1];
+
+	// Use this for initialization
+
+	private DisplayImage currentDisplay;
+
+	void Start()
+	{
+		currentDisplay = GameObject.Find("display").GetComponent<DisplayImage>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (button[0] = true && value[0] == 1)
+		{
+			espera();
+		}
+		time -= 1 * Time.deltaTime;
+		ctime = (int)time;
+		tempo.text = ctime.ToString();
+		if (time <= 0)
+		{
+			currentDisplay.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/gameover");
+		}
+
+	}
+
+	IEnumerator espera()
+	{
+		yield return new WaitForSeconds(2);
+	}
+
+}
+
